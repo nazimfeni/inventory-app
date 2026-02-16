b5af20e564370e7c982f305670649a49b564beb7
nazimjrf <nazim@jrfintel.com>
2026-02-16 14:43:26 +0600

    Inventory-App-Nextjs

---
+++ components/WelcomeCard.tsx
@@ -0,0 +1,8 @@
+export default function WelcomeCard() {
+return (
+<div className="mt-4 p-4 bg-green-100 border border-green-300 rounded text-green-800 mb-6">
+Welcome to your 30-Day Next.js Challenge!
+</div>
+);
+}
+
